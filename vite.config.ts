bbdf6dde3f75e229e2754a88da2382b7aab7fa0b
jakub <jakub.king1998@gmail.com>
2025-02-10 15:18:34 +0100

    #1

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/Ads-Panel-Recruitment-Task/",
   plugins: [react()],
 });
